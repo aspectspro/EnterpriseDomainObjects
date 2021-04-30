ee1db2d1dc8a1850abc06861fe3e7babad790726
greg <greg@aspectspro.com>
2021-04-29 16:04:31 -0800

    Added results to ParseQueryResponse

--- test/NewTest/Parse/parsequeryresponse.cpp
+++ test/NewTest/Parse/parsequeryresponse.cpp
@@ -1,1 +1,17 @@
 #include "parsequeryresponse.h"
+
+QJsonArray ParseQueryResponse::getResults() const
+{
+    return results;
+}
+
+void ParseQueryResponse::setResults(const QJsonArray &value)
+{
+    results = value;
+}
+
+const QMetaObject &ParseQueryResponse::metaObject() const
+{
+    return this->staticMetaObject;
+}
+
--- test/NewTest/Parse/parsequeryresponse.h
+++ test/NewTest/Parse/parsequeryresponse.h
@@ -4,7 +4,23 @@
 
 struct ParseQueryResponse : public ParseBaseRestResponse{
 
+    Q_GADGET
+    Q_PROPERTY(QJsonArray results READ getResults WRITE setResults)
+public:
+    QJsonArray getResults() const;
+    void setResults(const QJsonArray &value);
+
+
+private:
+    QJsonArray results;
+
+
+    // AbstractDomainObject interface
+public:
+    virtual const QMetaObject &metaObject() const override;
 };
+
+
 
 Q_DECLARE_METATYPE(ParseQueryResponse)
 
